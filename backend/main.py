b4f50af74d5fad8d8f62fdd2dd08658bd16ae251
kanishka-linux <kanishka.linux@gmail.com>
2025-03-10 03:27:25 +0530

    include route for submitting links and start async workers for processing urls on start

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.auth.router import router as auth_router
-from backend.auth.router import file_router
+from backend.auth.router import file_router, link_router
+from backend.auth.url_processor import process_url_queue
+from backend.auth.url_processor_recursive import process_recursive_url_queue
 from backend.config import settings
 from backend.database import create_db_and_tables
 from backend.core.logging import setup_logging
+import asyncio
 
 logger = setup_logging()
 
 # Create FastAPI app
-app = FastAPI(title="FastAPI JWT Auth")
+app = FastAPI(title="FastAPI Backend")
 
 # Configure CORS
 app.add_middleware(
@@ -25,17 +28,18 @@ app.add_middleware(
 # Include routers
 app.include_router(auth_router, prefix="/auth")
 app.include_router(file_router, prefix="/file")
-
-# Create database and tables on startup
+app.include_router(link_router, prefix="/links")
 
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+    asyncio.create_task(process_url_queue())
+    asyncio.create_task(process_recursive_url_queue())
 
 # Root endpoint
 
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to FastAPI JWT Auth API"}
+    return {"message": "Welcome to FastAPI API Backend"}
