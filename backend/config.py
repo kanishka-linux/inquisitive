bc3d5c3b4dad8159ae41ece64b63675f46a998ae
kanishka-linux <kanishka.linux@gmail.com>
2025-03-31 11:43:40 +0530

    update config for platform independent config directory creation

--- backend/config.py
+++ backend/config.py
@@ -3,11 +3,45 @@ from pydantic_settings import BaseSettings
 from pathlib import Path
 from typing import Dict
 from backend.vector_store.models import StoreEngine
+import os
 
 
 class Settings(BaseSettings):
-    # Database settings
-    DATABASE_URL: str = "sqlite+aiosqlite:///./inquisitive.db"
+    # BASIC DIRECTORY CREATION
+    HOME_DIR: str = os.path.expanduser("~")
+    CONFIG_DIR: str = os.path.join(HOME_DIR, ".config")
+    CONFIG_DIR_PATH: Path = Path(CONFIG_DIR)
+    CONFIG_DIR_PATH.mkdir(exist_ok=True)
+
+    BASE_DIR: str = os.path.join(CONFIG_DIR, "inquisitive")
+    BASE_DIR_PATH: Path = Path(BASE_DIR)
+    BASE_DIR_PATH.mkdir(exist_ok=True)
+
+    UPLOAD_DIR: str = os.path.join(BASE_DIR, "uploads")
+    UPLOAD_DIR_PATH: Path = Path(UPLOAD_DIR)
+    UPLOAD_DIR_PATH.mkdir(exist_ok=True)
+
+    # Sqlite Path
+    SQLITE_DB_PATH: str = os.path.join(BASE_DIR, "inquisitive.db")
+
+    # Vector store path
+    CHROMA_VECTOR_STORE_PERSISTS_DIRECTORY: str = os.path.join(
+        BASE_DIR, "chroma_db")
+    MILVUS_VECTOR_STORE_URL: str = os.path.join(BASE_DIR, "milvus_data.db")
+    LANCE_DB_VECTOR_STORE_PERSISTS_DIRECTOY: str = os.path.join(
+        BASE_DIR, "lance_db")
+
+    # Default vector database
+    DEFAULT_VECTOR_DB: str = StoreEngine.LANCE
+
+    VECTOR_STORE_COLLECTION_NAME: str = "document_store"
+
+    # Modify and use different embedding model if needed
+    EMBEDDINGS_MODEL: str = "chroma/all-minilm-l6-v2-f32"
+    EMBEDDINGS_DIMENSION: int = 384
+
+    # Modify if needed to use other relational DB
+    DATABASE_URL: str = f"sqlite+aiosqlite:///{SQLITE_DB_PATH}"
 
     # JWT settings
     # In production, use a secure key
@@ -21,21 +55,9 @@ class Settings(BaseSettings):
     # CORS settings
     # Streamlit default port
     CORS_ORIGINS: list[str] = ["http://localhost:8501"]
-
-    UPLOAD_DIR: Path = Path("./uploads")
-    UPLOAD_DIR.mkdir(exist_ok=True)
-    EMBEDDINGS_MODEL: str = "chroma/all-minilm-l6-v2-f32"
-    EMBEDDINGS_DIMENSION: int = 384
-
-    CHROMA_VECTOR_STORE_PERSISTS_DIRECTORY: str = "./chroma_db"
-    MILVUS_VECTOR_STORE_URL: str = "./milvus_data.db"
-    LANCE_DB_VECTOR_STORE_PERSISTS_DIRECTOY: str = "./lance_db"
-    VECTOR_STORE_COLLECTION_NAME: str = "document_store"
-    DEFAULT_VECTOR_DB: str = StoreEngine.LANCE
-
     WINDOW_SIZE_MULTIPLIER: int = 10
     DEFAULT_HEADERS: Dict[str, str] = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5"
     }
--- frontend/config.py
+++ frontend/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     ALLOW_AUTO_USER_REGISTER: bool = True
     UPLOAD_FILE_TYPES: list[str] = ['txt', 'pdf', "md", "json", "sh"]
     DEFAULT_HEADERS: Dict[str, str] = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5"
     }
